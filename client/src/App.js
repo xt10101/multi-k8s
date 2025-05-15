4164dc008c1cdf7cad756354a26cce2a79259710
Tom <tom.b.uk@hotmail.co.uk>
2025-05-15 13:37:15 +0100

    debugging

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Home test</Link>
+          <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
